7e646093a9ba39b3aa4b760237a0ab0090131b81
Alessandro Viganò <alvistar@gmail.com>
2018-01-10 15:28:30 +0100

    Improved logging. Added validation

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
+import logging
 import redis
 
+from jsonschema import validate, ValidationError
 from flask import Flask
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from usb_backend import PowBackend
+
+schema = {
+    "type": "object",
+    "properties": {
+        "hash": {"type": "string", "pattern": "[0-9A-Fa-f]{64}"}
+    }
+}
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
@@ -14,16 +25,23 @@ p = r.pubsub(ignore_subscribe_messages=True)
 
 @app.route('/', methods=['POST'])
 def post_json_handler():
-    print(request.is_json)
     content = request.get_json(force=True)
-    print(content)
+    logging.info('Received request:{}'.format(content))
+
+    try:
+        validate(content, schema)
+    except ValidationError as e:
+        logging.error('Received bad request:{}'.format(e))
+        abort(400)
+
     p.subscribe(content['hash'])
     r.publish('requests', content['hash'])
 
     for message in p.listen():
         break
 
-    print(message)
+    logging.debug('Received message from PowBackend:{}'.format(message))
+    logging.info('POW:{}'.format(message['data'].hex()))
     return jsonify(work=message['data'].hex())
 
 
--- usb_backend.py
+++ usb_backend.py
@@ -1,4 +1,5 @@
 import config
+import logging
 import redis
 import serial
 import threading
@@ -7,26 +8,28 @@ import threading
 class PowBackend:
     def __init__(self):
         self.ser = serial.Serial(config.SERIAL, 115200)
-        print(self.ser.name)
+        logging.info('Initialized serial {}'.format(self.ser.name))
 
         self.r = redis.StrictRedis(host='localhost')
         self.p = self.r.pubsub(ignore_subscribe_messages=True)
         self.p.subscribe('requests')
 
-        self.t = threading.Thread(target=self.worker,  daemon=True)
+        self.t = threading.Thread(target=self.
+                                  worker,  daemon=True)
         self.t.start()
 
     def worker(self):
         for message in self.p.listen():
-            print(message)
+            logging.debug('Got pow request:{}'.format(message))
+            # random bytes
             self.ser.write(b'12345678')
             # print('Written random')
             payload = bytes.fromhex(message['data'].decode('ascii'))
-            print("Hash:{}\nLen:{}".format(payload.hex(), len(payload)))
+            logging.debug("Hash:{} Len:{}".format(payload.hex(), len(payload)))
             self.ser.write(payload)
             # print('Written bytes')
             s = self.ser.read(20)
-            print("Output: {}".format(s.hex()))
+            logging.debug("FPGA Output: {}".format(s.hex()))
             self.r.publish(message['data'], s[8:16][::-1])
 
     def send_request(self, input_hash):
